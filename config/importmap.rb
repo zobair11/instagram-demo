2eb2448a9691b4f1a71b791b7672d4ae23ed9cd0
zobair11 <zobair619316@gmail.com>
2024-04-21 08:56:27 +0600

    login page

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap" # @5.3.3
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/esm/index.js" # @2.11.8
